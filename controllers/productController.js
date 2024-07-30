861d112290eea7ce940bcdf274cf548af6830b28
NilzakIndigo <nilzak.indigo05@gmail.com>
2024-07-30 13:39:38 +0100

    added product image

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,88 +1,97 @@
 const Product = require("../models/product");
 const mongoose = require("mongoose");
-
 const createProduct = async (req, res) => {
-    try {
-
-        if (!req.body.name) {
-            return res.status(400).json({
-                success: false,
-                message: "Veuillez entrer le nom de votre produit",
-            });
-        }
-        if (!req.body.price) {
-            return res.status(400).json({
-                success: false,
-                message: "Veuillez entrer le prix de votre produit",
-            });
-        }
-
-        const newProduct = new Product({
-            userId: req.user.userId,
-            ...req.body,
-        });
-
-        const createdProduct = await newProduct.save();
-
-        res.status(201).json(createdProduct);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Error creating Product" });
+  try {
+    if (!req.body.name) {
+      return res.status(400).json({
+        success: false,
+        message: "Veuillez entrer le nom de votre produit",
+      });
     }
+    if (!req.body.price) {
+      return res.status(400).json({
+        success: false,
+        message: "Veuillez entrer le prix de votre produit",
+      });
+    }
+
+    const newProduct = new Product({
+      user: req.user.userId,
+      ...req.body,
+    });
+
+    const createdProduct = await newProduct.save();
+
+    // Find the product again and populate the 'image' field
+    const populatedProduct = await Product.findById(
+      createdProduct._id
+    ).populate("image");
+
+    res.status(201).json(populatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error creating Product" });
+  }
 };
 
 const getProducts = async (req, res) => {
-    try {
-        const name = req.query.name;
-        const userId = req.user.userId; // Assuming you have the userId in req.user
+  try {
+    const name = req.query.name;
+    const user = req.query.user;
 
-        let products;
-        if (name) {
-            // Perform a case-insensitive search by name and exclude products created by the requesting user
-            products = await Product.find({ 
-                name: { $regex: new RegExp(name, "i") },
-            });
-        } else {
-            // If no name is provided, return all products except those created by the requesting user
-            products = await Product.find({});
-        }
+    // Initialize an empty query object
+    let query = {};
 
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching Products" });
+    // Add name condition if provided
+    if (name) {
+      query.name = { $regex: new RegExp(name, "i") };
     }
+
+    // Add user condition if provided
+    if (user) {
+      query.user = user;
+    }
+
+    // Find products based on the query and populate the image field
+    const products = await Product.find(query).populate("image");
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error fetching Products" });
+  }
 };
 
 const deleteProduct = async (req, res) => {
-    try {
-        const productId = req.params.id;
-        const userId = req.user.userId; // Assuming you have the userId in req.user
+  try {
+    const productId = req.params.id;
+    const userId = req.user.userId; // Assuming you have the userId in req.user
+    // Find the product to ensure it exists and was created by the requesting user
+    const product = await Product.findOne({ _id: productId, user: userId });
 
-        // Find the product to ensure it exists and was created by the requesting user
-        const product = await Product.findOne({ _id: productId, userId: userId });
-
-        if (!product) {
-            return res.status(404).json({
-                success: false,
-                message: "Product not found or you do not have permission to delete this product",
-            });
-        }
-
-        // Delete the product
-        await Product.deleteOne({ _id: productId });
-
-        res.status(200).json({
-            success: true,
-            message: "Product deleted successfully",
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Error deleting Product" });
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message:
+          "Product not found or you do not have permission to delete this product",
+      });
     }
+
+    // Delete the product
+    await Product.deleteOne({ _id: productId });
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error deleting Product" });
+  }
 };
 
 module.exports = {
-    createProduct,
-    getProducts,
-    deleteProduct,
+  createProduct,
+  getProducts,
+  deleteProduct,
 };
--- models/product.js
+++ models/product.js
@@ -1,29 +1,36 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const productSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        description: {
-            type: String,
-            required: true,
-        },
-        price: {
-            type: Number,
-            required: true,
-        },
-        brand: {
-            type: String,
-            required: true,
-        },
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true, versionKey: false }
+    description: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    image: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "File",
+      required: false,
+      default: null,
+    },
+  },
+  { timestamps: true, versionKey: false }
 );
 productSchema.plugin(mongoosePaginate);
 const Product = mongoose.model("Product", productSchema);
